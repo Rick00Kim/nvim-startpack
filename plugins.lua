1c4654d7cfc051690b2acd373bc9d2869e4ca253
Rick_Kim <dreamx119@gmail.com>
2023-11-19 23:50:03 +0900

    Create plugins.lua

---
+++ plugins.lua
@@ -0,0 +1,13 @@
+-- 📂lua/🌑plugins.lua
+
+-- Automatically run: PackerCompile
+vim.api.nvim_create_autocmd("BufWritePost", {
+    group = vim.api.nvim_create_augroup("PACKER", { clear = true }),
+    pattern = "plugins.lua",
+    command = "source <afile> | PackerCompile",
+})
+
+return require("packer").startup(function(use)
+    -- Packer
+    use("wbthomason/packer.nvim")
+end)
